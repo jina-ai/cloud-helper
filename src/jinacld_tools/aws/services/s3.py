c884309e599e235eb1f12935ceaf57c07576f15d
slettner <sebastian.lettner@jina.ai>
2021-05-03 13:41:44 +0200

    feat: add download function

--- src/jinacld_tools/aws/services/s3.py
+++ src/jinacld_tools/aws/services/s3.py
@@ -1,28 +1,37 @@
-from jinacld_tools.aws.client import AWSClientWrapper
-from jinacld_tools.aws.helper import file_exists
+import glob
+from pathlib import Path
+
 from jinacld_tools.aws.logger import get_logger
+from jinacld_tools.aws.client import AWSClientWrapper
 
 
 class S3:
-    """Wrapper around boto3 to upload to S3 bucket"""
+    """Wrapper around boto3 to upload to/download from S3 bucket
+    """
 
     def __init__(self, bucket: str):
-        self.logger = get_logger(context=self.__class__.__name__)
-        self._client_wrapper = AWSClientWrapper(service="s3")
-        self._client = self._client_wrapper.client
+        self._client = AWSClientWrapper(service='s3').client
         self._bucket = bucket
+        self._logger = get_logger(self.__class__.__name__)
 
-    def put(self, filepath, key):
-        if not file_exists(filepath):
-            self.logger.error(f"File {filepath} doesn't exist! Nothing to upload!")
+    def add(self, path: str, key: str):
+        if not Path(path).exists():
+            self._logger.error(f'Invalid path: {path}! Nothing to upload!')
             return
         try:
-            self.logger.info(
-                f"Uploading object from `{filepath}` to S3 bucket `{self._bucket}` key `{key}`"
-            )
-            with open(filepath, "rb") as data:
-                self._client.upload_fileobj(data, self._bucket, key)
+            self._logger.info(f'Uploading object from `{path}` to S3 bucket `{self._bucket}` key `{key}`')
+            for filename in glob.iglob(str(path) + '**/**', recursive=True):
+                if Path(filename).is_file():
+                    self._client.upload_file(filename, self._bucket, f'{key}/{filename}')
         except Exception as exp:
-            self.logger.error(
-                f"Got following exception while uploading object to S3 \n{exp}"
-            )
+            self._logger.error(f'Got following exception while uploading object to S3 \n{exp}')
+            raise
+
+    def get(self, key: str, local_path):
+        try:
+            self._logger.info(f'Downloading object from `{self._bucket}:{key}` to file: {local_path}')
+            # for filename in glob.iglob(path + '**/**', recursive=True):
+            self._client.download_file(self._bucket, key, local_path)
+        except Exception as exp:
+            self._logger.error(f'Got following exception while downloading object from S3 \n{exp}')
+            raise
