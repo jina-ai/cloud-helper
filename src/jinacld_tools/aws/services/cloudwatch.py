75246358e776b0321a25abb998fae84cf400b1ed
Kelton Zhang <zhuoran2@andrew.cmu.edu>
2021-05-11 15:33:55 +0800

    feat: add fetch metrics with endtime and s3 store

--- src/jinacld_tools/aws/services/cloudwatch.py
+++ src/jinacld_tools/aws/services/cloudwatch.py
@@ -24,14 +24,20 @@ class CloudWatch:
         self._client = self._client_wrapper.client
         self.metrics = "" # url of metrics collected
 
-    def fetch(self, ec2ARN):
+    def fetch(self, ec2ARN, endTime, metrics):
         '''
             given an ec2 instance's ARN, e.g. "arn:aws:ec2:region:accountID:instanceID
-            collect system metrics
+            collect system metrics from (launch time to user defined end time)
             - AWS provided : CPU, Disk, Network data, status check(VM instance and its hardware)
             - custom : RAM
         '''
         _, _, _, region, _, instanceID = ec2ARN.split(":")
+        '''
+        from boto3.ec2.connection import EC2Connection
+        conn = EC2Connection(ACCESS_KEY_ID, SECRET_ACCESS_KEY)
+        conn = EC2Connection()
+        print(f'conn: {conn}')'''
+
         #session = boto3.Session(profile_name="dev", region_name="us-east-1")
         '''
         ec2 = boto3.client('ec2', region_name="us-east-2", #aws_instance_id=
@@ -42,6 +48,8 @@ class CloudWatch:
 
         ec2 = boto3.resource('ec2', region_name=region,
                 aws_access_key_id=ACCESS_KEY_ID, aws_secret_access_key=SECRET_ACCESS_KEY)
+
+        instance = ec2.Instance(instanceID)
 
         for instance in ec2.instances.filter(Filters=[{'Name': 'instance-id', 'Values':[instanceID]}]):
             print(instance)
@@ -61,34 +69,44 @@ class CloudWatch:
         cloudwatch = boto3.client('cloudwatch', region_name=region,
                 aws_access_key_id=ACCESS_KEY_ID, aws_secret_access_key=SECRET_ACCESS_KEY)
 
-        response = cloudwatch.get_metric_statistics(
-            Namespace='AWS/EC2',
-            Dimensions=[
-                {
-                    'Name': 'instance-id',
-                    'Value': instanceID
-                },
-            ],
-            MetricName='CPUUtilization',
-            StartTime='2021-05-08T23:18:00', #datetime.now() - timedelta(days=7),
-            EndTime='2021-05-09T23:18:00', #datetime.now(),
-            Period=3600, #seconds in one hour
-            Statistics=[
-                'Maximum'
-            ]
-        )
+        responses = []
 
-        print(response)
+        for metric in metrics:
+            response = cloudwatch.get_metric_statistics(
+                Namespace='AWS/EC2',
+                Dimensions=[
+                    {
+                        'Name': 'InstanceId',
+                        'Value': instanceID
+                    },
+                ],
+                MetricName=metric,
+                StartTime=instance.launch_time,
+                EndTime=endTime,
+                Period=3600, #seconds in one hour
+                Statistics=[
+                    'Maximum'
+                ]
+            )
 
-    def store(self):
+            responses.append(response)
+
+        return responses
+
+    def store(self, metrics):
+        import pickle
+        serializedMetricsObject = pickle.dumps(metrics)
         # lambda call s3
         S3_DEFAULT_BUCKET = 'lambda-handlers-jina'
-        s3 = S3Bucket(bucket_name=S3_DEFAULT_BUCKET)
+        #s3 = S3Bucket(bucket_name=S3_DEFAULT_BUCKET)
         #s3.add(metrics, "metrics")
+        s3 = boto3.client('s3')
+        s3.put_object(Bucket=S3_DEFAULT_BUCKET, Key='EC2Metrics', Body=serializedMetricsObject)
 
-
+'''
 cloudwatcher = CloudWatch()
 ec2ARN = "arn:aws:ec2:us-east-2:"+accountID+":"+instanceID
-cloudwatcher.fetch(ec2ARN)
-cloudwatcher.store()
-
+endTime='2021-05-09T23:18:00'
+metrics = ['DiskReadOps', 'DiskWriteBytes', 'CPUUtilization']
+metricsCollected = cloudwatcher.fetch(ec2ARN, endTime, metrics)
+cloudwatcher.store(metricsCollected)'''
